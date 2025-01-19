00d86da1ecb7325d017fdca94273d322dc39df29
Tobias Waurick <tobtherock@proton.me>
2025-01-19 11:19:36 +0100

    Amd gpu settings

--- modules/amd.nix
+++ modules/amd.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }: {
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" "amdvlk" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ amdvlk ];
   };
+  boot.kernelParams = [ "video=DP-1:2560x1440@144" "video=DP-2:2560x1440@144" ];
 }
