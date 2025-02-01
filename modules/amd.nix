8f4638d1b4cf71f93fb7f7442c48594be728db31
Tobias Waurick <tobtherock@proton.me>
2025-02-01 14:18:43 +0100

    Fix luks boot

--- modules/amd.nix
+++ modules/amd.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }: {
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" "amdvlk" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [ amdvlk ];
   };
-  #  1080p and 30 Hz
-  boot.kernelParams = [ "video=DP-1:1920x1080@30" "video=DP-2:1920x1080@30" ];
-  # boot.kernelParams = [ "video=DP-1:3840x2160@60" "video=DP-2:3840x2160@60" ];
 }
--- modules/boot.nix
+++ modules/boot.nix
@@ -9,6 +9,12 @@
       };
     };
     initrd = {
+      systemd = {
+        enable = true;
+        fido2.enable = true;
+      };
+
+      kernelModules = [ "amdgpu" ];
       luks.devices = {
         root = {
           device = "/dev/nvme0n1p2";
@@ -17,5 +23,9 @@
         };
       };
     };
+    #  1080p and 30 Hz
+    kernelParams = [ "video=DP-1:1920x1080@30" "video=DP-2:1920x1080@30" ];
+    #   4K and 60 Hz
+    # kernelParams = [ "video=DP-1:3840x2160@60" "video=DP-2:3840x2160@60" ];
   };
 }
