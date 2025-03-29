45646be0290de5fd970917f9c06bb98670af12b5
Tobias Waurick <tobtherock@proton.me>
2025-03-29 22:06:13 +0100

    fix1 auto upgrade

--- modules/auto-upgrade.nix
+++ modules/auto-upgrade.nix
@@ -1,11 +1,40 @@
-{ config, ... }:
-let configDir = config.var.configDirectory;
+{ config, pkgs, ... }:
+let
+  username = config.var.username;
+  configDir = config.var.configDirectory;
+  flakePath = configDir + ".flake";
 in {
+  systemd.services = {
+    flake-update = {
+      preStart =
+        "${pkgs.host}/bin/host example.com"; # Check network connectivity
+      unitConfig = {
+        Description = "Update flake inputs";
+        StartLimitIntervalSec = 300;
+        StartLimitBurst = 5;
+      };
+      requiredBy = [ "nixos-upgrade.service" ];
+      serviceConfig = {
+        ExecStart =
+          "${pkgs.nix}/bin/nix flake update --commit-lock-file --flake ${flakePath}";
+        Restart = "on-failure";
+        RestartSec = "30";
+        Type =
+          "oneshot"; # Ensure that it finishes before starting nixos-upgrade
+        User = username;
+      };
+      before = [ "nixos-upgrade.service" ];
+      path = [ pkgs.nix pkgs.git pkgs.host ];
+    };
+  };
+
   system.autoUpgrade = {
-    enable = config.var.autoUpgrade;
-    dates = "04:00";
-    flake = "${configDir}";
-    flags = [ "--update-input" "nixpkgs" "--commit-lock-file" ];
+    enable = true;
+    dates = "03:00";
+    flake = flakePath;
     allowReboot = false;
   };
+
+  programs.git.config.safe.directory = [ flakePath ];
+  programs.git.enable = true;
 }
