f4819e658c6d46e2865e4d633b415824e125efae
Tobias Waurick <tobtherock@proton.me>
2025-07-11 17:25:13 +0200

    Add kb layout to hyprpanl

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -42,6 +42,7 @@ in {
               "volume"
               "bluetooth"
               "battery"
+              "kbinput"
               "network"
               "clock"
               "notifications"
