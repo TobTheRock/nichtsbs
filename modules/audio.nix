c5c56288d65ba0fa38ec7b1f43515d58ab181d67
Tobias Waurick <tobtherock@proton.me>
2026-07-27 14:23:49 +0200

    fix crackling audio with firefox

--- modules/audio.nix
+++ modules/audio.nix
@@ -9,5 +9,15 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber = { enable = true; };
+
+    # ponytail: bigger min-quantum fixes Firefox video crackling (buffer underruns).
+    # Lower back toward 256/512 if you hit audio latency in games/DAW.
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+      };
+    };
   };
 }
